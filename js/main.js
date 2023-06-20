c80fb4b12afacfe1e5198144c135357fe9951fe8
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-20 17:41:21 -0300

    passando parametros dinamicos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(id) {
+  document.querySelector(id).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,7 +7,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSomPom;
+
+  const tecla = listaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  } ;
 
   contador = contador + 1;
 
